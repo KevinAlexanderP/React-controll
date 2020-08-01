2fa2357e159adbcb9c32896f42e091a146f19f3c
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-08-01 17:32:39 -0500

    switch action on dcandidate

---
+++ src/Reducers/dCandidate.js
@@ -0,0 +1,11 @@
+import { ACTION TYPES } from "../Actions/DCandidate"
+export const dCandidate = (state = initialState.action ) =>{
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
